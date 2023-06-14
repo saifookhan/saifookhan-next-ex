7e1070ff82ad24304da34b8f9f8e80471c32ce69
saifookhan <isaifookhan@gmail.com>
2023-06-14 12:50:20 +0200

    add video list

---
+++ src/constants/germany/videoList.ts
@@ -0,0 +1,58 @@
+const videoList = [
+  {
+    chapter: "01",
+    chapterName: "Initial Research",
+    videos: [
+      {
+        title: "PR: Germany vs Italy for Masters",
+        description: "",
+        videoLink: "https://www.facebook.com/watch/?v=6117643851621824",
+        embedHtml:
+          '<iframe src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2Fsaifooexplores%2Fvideos%2F6117643851621824%2F&show_text=false&width=560&t=0" width="560" height="314" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>',
+      },
+      {
+        title: "Italy - Student Visa & Accomodation Problems",
+        description: "",
+        videoLink:
+          "https://www.youtube.com/watch?v=1ZcjSlzQEDs&ab_channel=SaifooExplores",
+        embedHtml:
+          '<iframe width="560" height="315" src="https://www.youtube.com/embed/1ZcjSlzQEDs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+      },
+      {
+        title: "Where can I find written Germany steps?",
+        description: "",
+        videoLink: "https://youtu.be/oXKbYX81d0M",
+        embedHtml:
+          '<iframe width="560" height="315" src="https://www.youtube.com/embed/oXKbYX81d0M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+      },
+    ],
+  },
+  {
+    chapter: "03",
+    chapterName: "Initial Research",
+    videos: [
+      {
+        title: "How to write a good motivation letter?",
+        description: "",
+        videoLink: "https://www.facebook.com/watch/?v=6117643851621824",
+        embedHtml:
+          '<iframe src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2Fsaifooexplores%2Fvideos%2F6117643851621824%2F&show_text=false&width=560&t=0" width="560" height="314" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>',
+      },
+      {
+        title: "Italy - Student Visa & Accomodation Problems",
+        description: "",
+        videoLink:
+          "https://www.youtube.com/watch?v=1ZcjSlzQEDs&ab_channel=SaifooExplores",
+        embedHtml:
+          '<iframe width="560" height="315" src="https://www.youtube.com/embed/1ZcjSlzQEDs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+      },
+      {
+        title: "Where can I find written Germany steps?",
+        description: "",
+        videoLink: "https://youtu.be/oXKbYX81d0M",
+        embedHtml:
+          '<iframe width="560" height="315" src="https://www.youtube.com/embed/oXKbYX81d0M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+      },
+    ],
+  },
+];
