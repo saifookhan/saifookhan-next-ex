4b4e7de3f32b362ebf860ae8e2df0dab841dcbe1
libzzola <123752659+libzzola@users.noreply.github.com>
2023-06-14 21:21:03 +0500

    updated section During Application

--- src/constants/germany/videoList.ts
+++ src/constants/germany/videoList.ts
@@ -33,33 +33,57 @@ const videoList = [
         embedHtml:
           '<iframe width="560" height="315" src="https://www.youtube.com/embed/vCNWVPiupkE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
       },
+      {
+        title: "Living Expenditure",
+        description: "",
+        videoLink:
+          "https://youtu.be/cRBZNAaq1DI",
+        embedHtml:
+          '<iframe width="560" height="315" src="https://www.youtube.com/embed/cRBZNAaq1DI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+      },
     ],
   },
   {
     chapter: "03",
-    chapterName: "Initial Research",
+    chapterName: "During Application",
     videos: [
+      {
+        title: "What is a motivation letter?",
+        description: "",
+        videoLink:
+          "https://youtu.be/Ec8tGmW-KnU",
+        embedHtml:
+          '<iframe width="560" height="315" src="https://www.youtube.com/embed/Ec8tGmW-KnU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+      },
       {
         title: "How to write a good motivation letter?",
         description: "",
-        videoLink: "https://www.facebook.com/watch/?v=6117643851621824",
+        videoLink: "https://youtu.be/Ec8tGmW-KnU",
         embedHtml:
-          '<iframe src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2Fsaifooexplores%2Fvideos%2F6117643851621824%2F&show_text=false&width=560&t=0" width="560" height="314" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>',
+          '<iframe width="560" height="315" src="https://www.youtube.com/embed/Ec8tGmW-KnU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>>',
       },
       {
-        title: "Italy - Student Visa & Accomodation Problems",
+        title: "Computer Science Masters Programs",
         description: "",
         videoLink:
-          "https://www.youtube.com/watch?v=1ZcjSlzQEDs&ab_channel=SaifooExplores",
+          "https://docs.google.com/spreadsheets/d/16KZutgOHyog4WHg5_ZRavQhJFBkYvrGd/edit?usp=sharing&ouid=104111435405880787554&rtpof=true&sd=true",
         embedHtml:
-          '<iframe width="560" height="315" src="https://www.youtube.com/embed/1ZcjSlzQEDs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+          'https://docs.google.com/spreadsheets/d/e/2PACX-1vSwEZqxvx8QawliTKroee15speFeQXFGRkh7DnRsStLN9nTuD7sBvmln9Vc7fFXDw/pubhtml?widget=true&amp;headers=false"></iframe>',
       },
       {
-        title: "Where can I find written Germany steps?",
+        title: "Business Masters Programs",
         description: "",
-        videoLink: "https://youtu.be/oXKbYX81d0M",
+        videoLink: "https://docs.google.com/spreadsheets/d/10YMNuT536jxU2tnwLSMZpATtUEdHo6PF/edit?usp=sharing&ouid=104111435405880787554&rtpof=true&sd=true",
         embedHtml:
           '<iframe width="560" height="315" src="https://www.youtube.com/embed/oXKbYX81d0M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+      },
+      {
+        title: "Scholarships in Germany",
+        description: "",
+        videoLink:
+          "https://drive.google.com/file/d/1ieMOxY2zUKo9m8FG3S0zq7pbc5QZj9rC/view?usp=sharing",
+        embedHtml:
+          'https://docs.google.com/spreadsheets/d/e/2PACX-1vSq9p8PRGgZfrnQUtQjPmVTf2gebCYp4FGRF8jR8hr4GbpesOk3W5zOb-2UMV9GJg/pubhtml?gid=59388856&amp;single=true&amp;widget=true&amp;headers=false"></iframe>',
       },
     ],
   },
