cb4c01f7b1bfa28afffc920c0f45fb53c6077147
Saifoo Khan <einstein.saifoo@gmail.com>
2023-07-11 18:04:05 +0200

    fix typos in videos link

--- src/constants/germany/videoList.ts
+++ src/constants/germany/videoList.ts
@@ -1,85 +1,86 @@
 const videoList = [
   {
-    chapter: '01',
-    chapterName: 'Initial Research',
-    description: 'All videos for basic know how.',
+    chapter: "01",
+    chapterName: "Initial Research",
+    description: "All videos for basic know how.",
     videos: [
       {
-        title: 'PR: Germany vs Italy for Masters',
-        description: '',
-        videoLink: 'https://www.facebook.com/watch/?v=6117643851621824',
-        embedHtml: 'https://www.youtube.com/embed/lOdZ3Gn_7Oc',
+        title: "PR: Germany vs Italy for Masters",
+        description: "",
+        videoLink: "https://www.facebook.com/watch/?v=6117643851621824",
+        embedHtml: "https://www.youtube.com/embed/lOdZ3Gn_7Oc",
       },
       {
-        title: 'Italy - Student Visa & Accomodation Problems',
-        description: '',
+        title: "Italy - Student Visa & Accomodation Problems",
+        description: "",
         videoLink:
-          'https://www.youtube.com/watch?v=1ZcjSlzQEDs&ab_channel=SaifooExplores',
-        embedHtml: 'https://www.youtube.com/embed/1ZcjSlzQEDs',
+          "https://www.youtube.com/watch?v=1ZcjSlzQEDs&ab_channel=SaifooExplores",
+        embedHtml: "https://www.youtube.com/embed/1ZcjSlzQEDs",
       },
       {
-        title: 'Where can I find written Germany steps?',
-        description: '',
-        videoLink: 'https://youtu.be/oXKbYX81d0M',
-        embedHtml: 'https://www.youtube.com/embed/oXKbYX81d0M',
+        title: "Where can I find written Germany steps?",
+        description: "",
+        videoLink: "https://youtu.be/oXKbYX81d0M",
+        embedHtml: "https://www.youtube.com/embed/oXKbYX81d0M",
       },
       {
-        title: 'Earning options as a student',
-        description: '',
+        title: "Earning options as a student",
+        description: "",
         videoLink:
-          'https://www.youtube.com/watch?v=vCNWVPiupkE&ab_channel=SaifooExplores',
-        embedHtml: 'https://www.youtube.com/embed/vCNWVPiupkE',
+          "https://www.youtube.com/watch?v=vCNWVPiupkE&ab_channel=SaifooExplores",
+        embedHtml: "https://www.youtube.com/embed/vCNWVPiupkE",
       },
       {
-        title: 'Living Expenditure',
-        description: '',
-        videoLink: 'https://youtu.be/cRBZNAaq1DI',
-        embedHtml: 'https://www.youtube.com/embed/cRBZNAaq1DI',
+        title: "Living Expenditure",
+        description: "",
+        videoLink: "https://youtu.be/cRBZNAaq1DI",
+        embedHtml: "https://www.youtube.com/embed/cRBZNAaq1DI",
       },
     ],
   },
   {
-    chapter: '03',
-    chapterName: 'During Application',
-    description: 'Once you have decided to study.',
+    chapter: "03",
+    chapterName: "During Application",
+    description: "Once you have decided to study.",
     videos: [
       {
-        title: 'What is a motivation letter?',
-        description: '',
-        videoLink: 'http://youtu.be/Ec8tGmW-KnU',
-        embedHtml: 'https://www.youtube.com/embed/Ec8tGmW-KnU',
+        title: "What is a motivation letter?",
+        description: "",
+        videoLink: "http://youtu.be/Ec8tGmW-KnU",
+        embedHtml: "https://www.youtube.com/embed/Ec8tGmW-KnU",
       },
       {
-        title: 'How to write a good motivation letter?',
-        description: '',
-        videoLink: 'https://youtu.be/Ec8tGmW-KnU',
-        embedHtml: 'https://www.youtube.com/embed/Ec8tGmW-KnU>',
+        title: "How to write a good motivation letter?",
+        description: "",
+        videoLink: "https://youtu.be/Ec8tGmW-KnU",
+        embedHtml: "https://www.youtube.com/embed/Ec8tGmW-KnU>",
       },
       {
-        title: 'Computer Science Masters Programs',
-        description: '',
+        title: "Computer Science Masters Programs",
+        description: "",
         videoLink:
-          'https://docs.google.com/spreadsheets/d/16KZutgOHyog4WHg5_ZRavQhJFBkYvrGd/edit?usp=sharing&ouid=104111435405880787554&rtpof=true&sd=true',
+          "https://docs.google.com/spreadsheets/d/16KZutgOHyog4WHg5_ZRavQhJFBkYvrGd/edit?usp=sharing&ouid=104111435405880787554&rtpof=true&sd=true",
         embedHtml:
-          'https://docs.google.com/spreadsheets/d/e/2PACX-1vSwEZqxvx8QawliTKroee15speFeQXFGRkh7DnRsStLN9nTuD7sBvmln9Vc7fFXDw/pubhtml?widget=true&amp;headers=false"></iframe>',
+          "https://docs.google.com/spreadsheets/d/16KZutgOHyog4WHg5_ZRavQhJFBkYvrGd/edit?usp=sharing&ouid=104111435405880787554&rtpof=true&sd=true",
       },
       {
-        title: 'Business Masters Programs',
-        description: '',
+        title: "Business Masters Programs",
+        description: "",
         videoLink:
-          'https://docs.google.com/spreadsheets/d/10YMNuT536jxU2tnwLSMZpATtUEdHo6PF/edit?usp=sharing&ouid=104111435405880787554&rtpof=true&sd=true',
-        embedHtml: 'https://www.youtube.com/embed/oXKbYX81d0M',
-      },
-      {
-        title: 'Scholarships in Germany',
-        description: '',
-        videoLink:
-          'https://drive.google.com/file/d/1ieMOxY2zUKo9m8FG3S0zq7pbc5QZj9rC/view?usp=sharing',
+          "https://docs.google.com/spreadsheets/d/10YMNuT536jxU2tnwLSMZpATtUEdHo6PF/edit?usp=sharing&ouid=104111435405880787554&rtpof=true&sd=true",
         embedHtml:
-          'https://docs.google.com/spreadsheets/d/e/2PACX-1vSq9p8PRGgZfrnQUtQjPmVTf2gebCYp4FGRF8jR8hr4GbpesOk3W5zOb-2UMV9GJg/pubhtml?gid=59388856&amp;single=true&amp;widget=true&amp;headers=false"></iframe>',
+          "https://docs.google.com/spreadsheets/d/10YMNuT536jxU2tnwLSMZpATtUEdHo6PF/edit?usp=sharing&ouid=104111435405880787554&rtpof=true&sd=true",
+      },
+      {
+        title: "Scholarships in Germany",
+        description: "",
+        videoLink:
+          "https://drive.google.com/file/d/1ieMOxY2zUKo9m8FG3S0zq7pbc5QZj9rC/view?usp=sharing",
+        embedHtml:
+          "https://drive.google.com/file/d/1ieMOxY2zUKo9m8FG3S0zq7pbc5QZj9rC/view?usp=sharing",
       },
     ],
   },
-]
+];
 
-export default videoList
+export default videoList;
