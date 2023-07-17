5f908dc6d4d3ee2ddaa3aec2326ba79c1a172c01
Saifoo Khan <einstein.saifoo@gmail.com>
2023-07-17 16:08:49 +0200

    update live group link

--- src/components/germany/SocialIcons.tsx
+++ src/components/germany/SocialIcons.tsx
@@ -1,65 +1,73 @@
-import React from 'react'
-import Image from 'next/image'
-import { Card } from 'antd'
+import React from "react";
+import Image from "next/image";
+import { Card } from "antd";
 
-import youtubeIcon from '../../../public/logos/youtube.svg'
-import whatsappIcon from '../../../public/logos/whatsapp.svg'
-import facebookIcon from '../../../public/logos/facebook.svg'
-import Link from 'antd/es/typography/Link'
+import youtubeIcon from "../../../public/logos/youtube.svg";
+import whatsappIcon from "../../../public/logos/whatsapp.svg";
+import facebookIcon from "../../../public/logos/facebook.svg";
+import Link from "antd/es/typography/Link";
 
 const gridStyle: React.CSSProperties = {
-  width: '20%',
-  textAlign: 'center',
-  padding: '12px',
-}
+  width: "20%",
+  textAlign: "center",
+  padding: "12px",
+};
 const gridStyleTwo: React.CSSProperties = {
-  width: '80%',
-  textAlign: 'left',
-  padding: '12px',
-}
+  width: "80%",
+  textAlign: "left",
+  padding: "12px",
+};
 
 const socialChannels = [
   {
     image: whatsappIcon,
-    imageAlt: 'Study in Germany/EU',
-    title: 'Study in Germany/EU',
-    subTitle: '894 members',
-    type: 'Whatsapp',
-    link: 'https://chat.whatsapp.com/DPCryPTKbIX6NHJBw0qeOw',
+    imageAlt: "Study in Germany/EU",
+    title: "Study in Germany/EU",
+    subTitle: "894 members",
+    type: "Whatsapp",
+    link: "https://chat.whatsapp.com/DPCryPTKbIX6NHJBw0qeOw",
   },
   {
     image: whatsappIcon,
-    imageAlt: 'Work Germany/EU',
-    title: 'Work Germany/EU',
-    subTitle: '569 members',
-    type: 'Whatsapp',
-    link: 'https://chat.whatsapp.com/J6WeoQ4kSlF4k6WoYcm81w',
+    imageAlt: "Work Germany/EU",
+    title: "Work Germany/EU",
+    subTitle: "569 members",
+    type: "Whatsapp",
+    link: "https://chat.whatsapp.com/J6WeoQ4kSlF4k6WoYcm81w",
   },
   {
     image: whatsappIcon,
-    imageAlt: 'Study in US/UK/Canada/Australia',
-    title: 'Study in US/UK/Canada/Australia',
-    subTitle: '193 members',
-    type: 'Whatsapp',
-    link: 'https://chat.whatsapp.com/Dlud50ZwbAL6bdzhQpxRao',
+    imageAlt: "Live Q&A Sessions Invite",
+    title: "Live Q&A Sessions Invite",
+    subTitle: "754 members",
+    type: "Whatsapp",
+    link: "https://chat.whatsapp.com/LYUxWCErPtUG5HdMOdUjky",
+  },
+  {
+    image: whatsappIcon,
+    imageAlt: "Study in US/UK/Canada/Australia",
+    title: "Study in US/UK/Canada/Australia",
+    subTitle: "193 members",
+    type: "Whatsapp",
+    link: "https://chat.whatsapp.com/Dlud50ZwbAL6bdzhQpxRao",
   },
   {
     image: facebookIcon,
-    imageAlt: 'Work/Study in EU/Germany - Support Community',
-    title: 'Work/Study in EU/Germany - Support Community',
-    subTitle: '3.8K members',
-    type: 'Facebook',
-    link: 'https://www.facebook.com/groups/saifooexplores.community',
+    imageAlt: "Work/Study in EU/Germany - Support Community",
+    title: "Work/Study in EU/Germany - Support Community",
+    subTitle: "3.8K members",
+    type: "Facebook",
+    link: "https://www.facebook.com/groups/saifooexplores.community",
   },
   {
     image: youtubeIcon,
-    imageAlt: 'SaifooExplores - YouTube Channel',
-    title: 'SaifooExplores - YouTube Channel',
-    subTitle: '2.5K subscribers',
-    type: 'YouTube',
-    link: 'https://openinyoutube.com/@saifooexplores',
+    imageAlt: "SaifooExplores - YouTube Channel",
+    title: "SaifooExplores - YouTube Channel",
+    subTitle: "2.5K subscribers",
+    type: "YouTube",
+    link: "https://openinyoutube.com/@saifooexplores",
   },
-]
+];
 
 function SocialIcons() {
   return (
@@ -86,12 +94,12 @@ function SocialIcons() {
                 </a>
               </Card.Grid>
             </>
-          )
+          );
         })}
-        <Card.Grid style={{ width: '100%' }}>Click on icon to open.</Card.Grid>
+        <Card.Grid style={{ width: "100%" }}>Click on icon to open.</Card.Grid>
       </Card>
     </>
-  )
+  );
 }
 
-export default SocialIcons
+export default SocialIcons;
